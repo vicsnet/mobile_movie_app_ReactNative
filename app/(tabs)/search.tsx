72ae2fdfca417c76e63ee88721efee0762219398
vicsnet <adesanmiadebisi@gmail.com>
2026-01-15 16:15:11 +0000

    chore: search componet updated

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
 
 const Search = () => {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const { data: movies, loading: moviesLoading, error: moviesError, refetch, reset } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await refetch();
+      } else {
+        reset();
+      }
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className='flex-1 absolute w-full z-0 ' resizeMode='cover' />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className='px-5'
+        columnWrapperStyle={{
+          justifyContent: 'center',
+          gap: 16,
+          marginVertical: 16,
+
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+
+        ListHeaderComponent={
+          <>
+            <View className='w-full flex-row justify-center mt-20 items-center'>
+              <Image source={icons.logo} className='w-12 h-10 ' />
+            </View>
+
+            <View className='my-5'>
+              <SearchBar placeholder='Search movies ...' value={searchQuery} onChangeText={(text: string) => setSearchQuery(text)} />
+
+            </View>
+
+            {
+              moviesLoading && (
+                <ActivityIndicator size="large" color="#0000ff" className='my-3' />
+              )
+            }
+
+            {
+              moviesError && (
+                <Text className='text-white text-lg font-bold my-3'> Error: {moviesError} </Text>
+              )
+            }
+
+            {
+              !moviesLoading && !moviesError && searchQuery.trim() && movies?.length > 0 && (
+
+                <Text className='text-xl text-white font-bold'>
+                  Search Result for {' '}
+                  <Text className='text-accent font-bold'>
+                    {searchQuery}
+                  </Text>
+                </Text>
+              )
+            }
+          </>
+        }
+        ListEmptyComponent={
+          !moviesLoading && !moviesError ? (
+
+            <View className='mt-10 px-5'>
+              <Text className='text-center text-gray-500 '>
+                {searchQuery.trim() ? 'No movies found.' : 'Start typing to search for movies.'}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
     </View>
   )
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,18 +4,20 @@ import { icons } from '@/constants/icons'
 
 interface props {
     placeholder: string;
-    onPress: () => void;
+    value?:string;
+    onChangeText?: (text:string) => void;
+    onPress?: () => void;
 }
 
-const SearchBar = ({placeholder, onPress}:props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText}:props) => {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=''
-                onChangeText={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor='#a8b5db'
                 className='flex-1 ml-2 text-white'
             />
